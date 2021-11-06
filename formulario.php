d092ba7de58f0ab58a93fbd8f34a1843f501234a
Straffon18 <Davidstraffgus@gmail.com>
2021-11-05 18:20:33 -0600

    .

--- formulario.php
+++ formulario.php
@@ -5,8 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< Updated upstream
     <link rel="stylesheet" href="css/formulario.css">
     <title>Formulario registro</title>
+=======
+    <link rel="stylesheet" href="intentos.css">
+    <title>Formulario</title>
+>>>>>>> Stashed changes
 </head>
 
 <body>
@@ -15,6 +20,7 @@
             <center>Formulario de problema</center>
         </h4>
 
+<<<<<<< Updated upstream
         <input class="controls" type="text" name="id" id="id" placeholder="Ingrese su ID">
         <br><br>
         <form class="controls" name="problema" id="problema">
@@ -78,9 +84,37 @@
         </div>
 
 
+=======
+            <form class="controls" name="select" id="select" method="get" >
+                Seleccione problema
+                <select class="seleccion">
+                    <option disabled selected="">opciones a escojer</option>
+                    <option class="op">Virus informatico</option>
+                    <option class="op">Computadora lenta</option>
+                    <option class="op">Error </option>
+                    <option class="op">Armado de PC</option>
+                    <option class="op">Sale humo</option>
+                </select>
+            </form>
+            <br>
+
+            <form  method="get" target="_blank"> 
+                <imput class="controls" type="Color" name="colorField" value="#FFDD00"> 
+                <input class="controls" type="date" name="dataField" value="2021-02-25"> <br> <br>
+                <input class="controls" type="datetime-local" name="dtlField" value="2021-11-05TL2:00">
+            </form>
+            
+            <br><br>
+        
+            <div>
+                <p>Estoy de acuerdo con <a href="#">Terminos y condiciones</a></p>
+                <a href="#" class="flecha izq"></a>
+                <a href="#" class="flecha right"></a>
+            </div>
+        
+>>>>>>> Stashed changes
     </section>
 
-    </div>
 </body>
 
 </html>
