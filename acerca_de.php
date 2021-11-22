4071c174ccd47b284f42fafb83c6f8926799a0fb
Gera1QWA <91513443+Gera1QWA@users.noreply.github.com>
2021-11-21 21:38:28 -0600

    cambiosgera2

--- acerca_de.php
+++ acerca_de.php
@@ -1,22 +1,111 @@
 <?php require 'partials/header.php' ?>
     
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@300;600&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/inicio_sesion.css">
-</head>
 
-</body>
-    <div>
-        hola
-    </div>
+<div class="container">
 
-</html>
+            <div class="container">
+                <div class="p-4 p-md-5">
+                <div id="MiCarrusel" class="carousel slide" data-bs-ride="carousel">
+                                    <div class="carousel-indicators">
+                                        <button type="button" class="active" data-bs-target="#MiCarrusel" data-bs-slide-to="0" arial-label="Slide 0"></button>
+                                        <button type="button" data-bs-target="#MiCarrusel" data-bs-slide-to="1" arial-label="Slide 1"></button>
+                                        <button type="button" data-bs-target="#MiCarrusel" data-bs-slide-to="2" arial-label="Slide 2"></button>
+                                    </div>
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item active">
+                                            <img src="images/citascarrucel2.jpg">
+                                            <div class="container">
+                                                <div class="carousel-caption text-start">
+                                                    <div class="fondo">
+                                                        <h1>Servicio de mantenimiento</h1>
+                                                    </div>
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="images/citascarrucel1.jpg">
+                                            <div class="container">
+                                                <div class="carousel-caption">
+                                                    <div class="fondo">
+                                                        <h1>Solucionamos problemas urgentes</h1>
+                                                        
+                                                    </div>
+                                                    <p><a href="citas-programar.php" class="btn btn-sm btn-primary">hacer citas</a></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="images/citascarrucel3.jpg">
+                                            <div class="container">
+                                                <div class="carousel-caption text-end">
+                                                    <div class="fondo">
+                                                        <h1>Servicio de manteniminto preventivo y presupuestos</h1>
+                                                        <p>Hacemos presupuestos para tus equipos y reviciones pedidicas </p>
+                                                    </div>
+                                                    <p><a href="citas-programar.php" class="btn btn-sm btn-primary">informacion</a></p>
+                                                </div>
+                                            </div>
+                                        </div>						
+                                    </div>
+                                    <button class="carousel-control-prev" type="button" data-bs-target="#MiCarrusel" data-bs-slide="prev">
+                                        <span class="carousel-control-prev-icon"></span>
+                                        <span class="visually-hidden">Previous</span>
+                                    </button>
+                                    <button class="carousel-control-next" type="button" data-bs-target="#MiCarrusel" data-bs-slide="next">
+                                        <span class="carousel-control-next-icon"></span>
+                                        <span class="visually-hidden">Next</span>
+                                    </button>					
+                                </div>
+                </div>
+                
+            </div>
+            <div class="conteiner">
+                    <div class="p-4 p-md-5 mb-4 my-4 text-white bg-dark forma">
+                                        <div class="acd1 col-12">
+                                            <h1 class="texto display-4 fst-italic">El mejor servicio de reparacion en la web</h1>
+                                            <p class="lead my-3">Contamos con la mejor garantia para tus reparaciones nuestro servicio de citas es rapido y eficas en el encotraras a una ideal para tus horarios, junto con el mantenimiento que requieras</p>
+                                            <p class="lead" align="left"><a href="citas-programar.php" class="text-white fw-bold">haz una cita ....</a></p>
+                                        </div>
+                                    </div>	 
+                                    
+                </div>
+                <div>
+                <div class="row">
+						<div class="col-12 col-sm-6 col-md-4 col-lg-4">
+							<div class="caja">
+								<i class="fa fa-instagram fa-4x icono"></i>
+								<h4>Instagram</h4>
+								<div>
+									<span>Aqui puedes encontrar el enlace a los comentarios realizados via <strong>Instagram</strong></span>
+								</div>
+								<a href="#" class="link" >Acceso</a>
+							</div>
+						</div>
+
+						<div class="col-12 col-sm-6 col-md-4 col-lg-4">
+							<div class="caja">
+								<i class="fa fa-twitter fa-4x icono"></i>
+								<h4>Twitter</h4>
+								<div>
+									<span>Aqui puedes encontrar el enlace a los comentarios realizados via <strong>Twitter</strong></span>
+								</div>
+								<a href="#" class="link" >Acceso</a>
+							</div>
+						</div>
+                        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+							<div class="caja">
+								<i class="fa fa-pinterest fa-4x icono"></i>
+								<h4>Pinterest</h4>
+								<div>
+									<span>Aqui puedes encontrar el enlace a los comentarios realizados via <strong>Pinterest</strong></span>
+								</div>
+								<a href="#" class="link" >Acceso</a>
+							</div>
+						</div>
+                </div>
+</div>
+
 
 <?php require 'partials/footer.php' ?>
